ab5377833abaa9e40b09db75a7690f37fb279c16
Md Noornawaz Rahman <noornawazrahman@gmail.com>
2025-10-20 15:35:43 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({ fallback: '404.html' }),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? base : ''
+			base: ''
 		}
 	}
 };
