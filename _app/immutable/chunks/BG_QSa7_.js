bf8143fa66301884611583592f3824dfaaf230e7
NoornawazRahman <noornawazrahman@gmail.com>
2025-11-11 13:33:50 +0000

    Deploying to gh-pages from @ NoornawazRahman/portfolio-svelte@6db78bfa0b5a87550dff1dcb6ac415beb4cbbe83 🚀

--- _app/immutable/chunks/BG_QSa7_.js
+++
@@ -1,7 +0,0 @@
-import"./BaHCyGMA.js";import{a as t}from"./ZljZnnAO.js";var i=(e=>(e.FullTime="Full-time",e.PartTime="Part-time",e.SelfEmployed="Self-employed",e.Freelance="Freelance",e.Contract="Contract",e.Internship="Internship",e))(i||{});const n="Experience",a=e=>({light:e,dark:e}),o=`Developed DigiCertify, a web application for generating and digitally signing certificates.
-
-Built backend using Spring Boot and iText for dynamic PDF generation and digital signatures.
-
-Designed frontend using Thymeleaf to create a user-friendly certificate generation interface.
-
-Improved automation and reduced manual work in certificate creation.`,r="https://upload.wikimedia.org/wikipedia/en/thumb/a/ac/IIEST_Shibpur_Logo.svg/250px-IIEST_Shibpur_Logo.svg.png",s=[{slug:"internship",company:"IIEST Shibpur",description:o,contract:i.Internship,type:"Software Development",location:"Howrah",period:{from:new Date(2025,4,19),to:new Date(2025,6,20)},skills:t("ts","js","nodejs","html","css","thymeleaf","springboot","java","reactjs"),name:"Research & Development Intern",color:"red",links:[],logo:a(r),shortDescription:"Web Based Document Authentication with Digital Signature"}],g={title:n,items:s};export{g as E};
